9dc24b9b9c9d6798a537d43985e6c1b55cd2cdc9
Nitin Verma <nitinvermanv61506@gmail.com>
2024-02-29 20:01:57 +0530

    nothing special

--- src/app/pages/Logout/page.tsx
+++ src/app/pages/Logout/page.tsx
@@ -1,17 +1,19 @@
-import { Typography } from '@mui/material'
+import { Typography, Button } from "@mui/material";
+import Link from "next/link";
 
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div className='text-center mt-32'>
-     
-       <Typography variant='h1'>Thanks for visiting</Typography>
-      
-        
-      
+    <div className="text-center mt-32">
+      <Typography variant="h1">Thanks for visiting</Typography>
+      <button>
+        <Link href="https://www.google.com">
+          GO TO GOOGLE
+        </Link>
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
